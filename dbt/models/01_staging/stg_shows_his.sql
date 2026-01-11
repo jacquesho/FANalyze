ba0c4a65c62743c2ddc7211352d13320464bc193
Jacques Ho <jacquesho@gmail.com>
2026-01-11 15:58:55 +0700

    test-ci-workflow #11

--- dbt/models/01_staging/stg_shows_his.sql
+++ dbt/models/01_staging/stg_shows_his.sql
@@ -16,16 +16,16 @@ cleaned_data as (
         -- Dates
         case
             when show_date like '%/%'
-            then
-                try_cast(
-                    concat(
-                        split_part(show_date, '/', 3),
-                        '-',
-                        lpad(split_part(show_date, '/', 1), 2, '0'),
-                        '-',
-                        lpad(split_part(show_date, '/', 2), 2, '0')
-                    ) as date
-                )
+                then
+                    try_cast(
+                        concat(
+                            split_part(show_date, '/', 3),
+                            '-',
+                            lpad(split_part(show_date, '/', 1), 2, '0'),
+                            '-',
+                            lpad(split_part(show_date, '/', 2), 2, '0')
+                        ) as date
+                    )
             else try_cast(show_date as date)
         end as show_date,
 
--- dbt/models/01_staging/test_ci.md
+++ dbt/models/01_staging/test_ci.md
@@ -1,2 +1,1 @@
 # CI test
-# CI test 2
